1646d0ac3121a80a0a6faade0d00cfb382d4e322
Anjana Nair <anjuviswan93@gmail.com>
2020-02-04 18:10:57 +0530

    simple counter to check reactjs

--- src/App.js
+++ src/App.js
@@ -2,25 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      meaningOfLife : 47
+    }
+  }
+  handleChange = () =>
+  {
+    this.setState({meaningOfLife : this.state.meaningOfLife+1},
+      () =>
+    {
+    console.log(this.state.meaningOfLife);
+
+    })
+  }
+  render(){
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        { this.state.meaningOfLife }
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick = { this.handleChange }>
+        Update Number
+        </button>
       </header>
     </div>
   );
+}
 }
 
 export default App;
