0d326cab807368a71625e5efa147916973742ce8
Anjana Nair <anjuviswan93@gmail.com>
2020-02-07 00:09:34 +0530

    directory without styling

--- src/App.js
+++ src/App.js
@@ -1,38 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HomePage  from './homepage.component.jsx';
 
-class App extends React.Component {
-  constructor(){
-    super()
-    this.state = {
-      meaningOfLife : 47
-    }
-  }
-  handleChange = () =>
-  {
-    this.setState({meaningOfLife : this.state.meaningOfLife+1},
-      () =>
-    {
-    console.log(this.state.meaningOfLife);
-
-    })
-  }
-  render(){
+function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        { this.state.meaningOfLife }
-        </p>
-        <button onClick = { this.handleChange }>
-        Update Number
-        </button>
-      </header>
+    <HomePage />
     </div>
   );
-}
 }
 
 export default App;
