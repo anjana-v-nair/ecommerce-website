cc7dc246407024242a5a0408be6c7a5f6655b234
Anjana Nair <anjuviswan93@gmail.com>
2020-02-09 00:14:44 +0530

    collection preview list added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from 'react'
 import './App.css'
-import HomePage  from './pages/homepage/homepage.component.jsx'
 import { Route, Switch, Link } from 'react-router-dom'
 
-const HatsPage = (props) => {
-  console.log(props)
-  return(
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)}
+import HomePage  from './pages/homepage/homepage.component'
+import ShopPage  from './pages/shop/shop.component'
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path='/' component={ HomePage } />
-        <Route  exact path='/hats' component={ HatsPage } />
+        <Route  path='/shop' component={ ShopPage } />
       </Switch>
     </div>
   );
